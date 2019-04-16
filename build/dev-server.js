0e139cd8446ae37135993f92d8f9e78eb9a65731
LINI5 <nickkcik.li@oocl.com>
2019-04-16 16:27:02 +0800

    remove the frame

--- build/dev-server.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-// console.log('111');
-const app = require('../lib/app');
-const webpack = require('./webpack.build');
-app.start(function (app) {
-  webpack.packageEnv(app);
-  console.log('app is up');
-});
--- server/routes/app.server.routes.js
+++
@@ -1,5 +0,0 @@
-const core = require('../controllers/core.server.controller');
-
-module.exports = function (app) {
-  app.route('/').get(core.renderHomePage);
-};
